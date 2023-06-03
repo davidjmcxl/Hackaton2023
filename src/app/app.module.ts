8e73d6f73e22817839e4524cfdbee004cca4d550
David Moreno <davidjmcxl@gmail.com>
2023-06-03 17:07:57 -0500

    primer avance lading page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { DescriptionComponent } from './description/description.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    DescriptionComponent
   ],
   imports: [
     BrowserModule
